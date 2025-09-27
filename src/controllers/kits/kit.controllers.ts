64ee52f68bf6441c2045f91586096eea2a954169
progFilthi <gamerfilthi@gmail.com>
2025-09-27 21:37:48 +0200

    Implement CRUD operations for kits

--- src/controllers/kits/kit.controllers.ts
+++ src/controllers/kits/kit.controllers.ts
@@ -5,22 +5,41 @@ export const kitControllers = {
   getAllkits: async (_: Request, res: Response) => {
     try {
       const kits = await prisma.kit.findMany();
-      res.status(200).json({ kits: kits });
+      res.status(200).json({ kits });
     } catch (error) {
       console.error("Internal server error while fetching kits", error);
       res
         .status(500)
-        .json({ Message: "Internal server error while fetching kits" });
+        .json({ message: "Internal server error while fetching kits" });
     }
   },
+
+  getKit: async (req: Request, res: Response) => {
+    try {
+      const { kitId } = req.params;
+      if (!kitId) return res.status(400).json({ message: "Kit ID is missing" });
+
+      const kit = await prisma.kit.findUnique({ where: { id: kitId } });
+      if (!kit) return res.status(404).json({ message: "Kit not found" });
+
+      res.status(200).json({ kit });
+    } catch (error) {
+      console.error("Internal server error while fetching a kit", error);
+      res
+        .status(500)
+        .json({ message: "Internal server error while fetching a kit" });
+    }
+  },
+
   createKit: async (req: Request, res: Response) => {
     try {
       const { title, description, price, audioKey, imageKey } = req.body;
 
-      if (!title || !price || !audioKey || !imageKey)
+      if (!title || !price || !audioKey || !imageKey) {
         return res.status(400).json({
-          Message: "Title, price, audioKey & imageKey are required",
+          message: "Title, price, audioKey, and imageKey are required",
         });
+      }
 
       const baseUrl = `https://${process.env.S3_BUCKET}.s3.${process.env.AWS_REGION}.amazonaws.com`;
 
@@ -34,10 +53,51 @@ export const kitControllers = {
         },
       });
 
-      res.status(201).json({ Message: "Kit created successfully", kit });
+      res.status(201).json({ message: "Kit created successfully", kit });
     } catch (error) {
       console.error("Internal server error while creating a kit", error);
-      res.status(500).json({ Message: "Internal server error" });
+      res.status(500).json({ message: "Internal server error" });
+    }
+  },
+
+  updateKit: async (req: Request, res: Response) => {
+    try {
+      const { kitId } = req.params;
+      const { title, description, price } = req.body;
+
+      if (!kitId) return res.status(400).json({ message: "Kit ID is missing" });
+
+      const kit = await prisma.kit.update({
+        where: { id: kitId },
+        data: {
+          title,
+          description,
+          price,
+        },
+      });
+
+      res.status(200).json({ message: "Kit updated successfully", kit });
+    } catch (error) {
+      console.error("Internal server error while updating a kit", error);
+      res
+        .status(500)
+        .json({ message: "Internal server error while updating a kit" });
+    }
+  },
+
+  deleteKit: async (req: Request, res: Response) => {
+    try {
+      const { kitId } = req.params;
+      if (!kitId)
+        return res.status(400).json({ message: "Kit ID is required" });
+
+      const kit = await prisma.kit.delete({ where: { id: kitId } });
+      res.status(200).json({ message: "Kit deleted successfully", kit });
+    } catch (error) {
+      console.error("Internal server error while deleting a kit", error);
+      res
+        .status(500)
+        .json({ message: "Internal server error while deleting a kit" });
     }
   },
 };
--- src/routes/kits/kit.routes.ts
+++ src/routes/kits/kit.routes.ts
@@ -4,4 +4,7 @@ import { kitControllers } from "../../controllers/kits/kit.controllers";
 export const kitRoutes = Router();
 
 kitRoutes.get("/", kitControllers.getAllkits);
+kitRoutes.get("/:kitId", kitControllers.getKit);
 kitRoutes.post("/", kitControllers.createKit);
+kitRoutes.put("/:kitId", kitControllers.updateKit);
+kitRoutes.delete("/kitId", kitControllers.deleteKit);
